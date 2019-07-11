9e76ae75a93f1fc447115bcc9d4510d6cd69ddff
idohalevi <ido.h@logz.io>
2019-07-11 11:28:34 +0300

    change naming in tests

--- tests/modules/test_docker.py
+++ tests/modules/test_docker.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from modules.docker.docker import setup, MODULE_NAME
+from modules.docker.docker import setup
 
 
 class TestDockerModule(unittest.TestCase):
@@ -51,15 +51,15 @@ class TestDockerModule(unittest.TestCase):
         #     }
         # ]
         try:
-            containers_str = os.environ["dockerSkipContainerName"] = "container0, container1, container2"
+            containers_str = os.environ["DOCKER_SKIP_CONTAINER_NAME"] = "container0, container1, container2"
             exclude_containers = [container.strip() for container in containers_str.split(",")]
 
-            conf, name = setup()
+            conf = setup()
             exclude_path = conf[0]["processors"][0]["drop_event"]["when"]["or"]
             for x in range(len(exclude_containers)):
                 self.assertEqual(exclude_containers[x], exclude_path[x]["contains"]["docker.container.name"])
         finally:
-            del os.environ["dockerSkipContainerName"]
+            del os.environ["DOCKER_SKIP_CONTAINER_NAME"]
 
     def test_include_containers(self):
         # [
@@ -113,63 +113,63 @@ class TestDockerModule(unittest.TestCase):
         #     }
         # ]
         try:
-            containers_str = os.environ["dockerMatchContainerName"] = "container0, container1, container2"
+            containers_str = os.environ["DOCKER_MATCH_CONTAINER_NAME"] = "container0, container1, container2"
             include_containers = [container.strip() for container in containers_str.split(",")]
 
-            conf, name = setup()
+            conf = setup()
             include_path = conf[0]["processors"][0]["drop_event"]["when"]["and"]
             for x in range(len(include_containers)):
                 self.assertEqual(include_containers[x], include_path[x]["not"]["contains"]["docker.container.name"])
         finally:
-            del os.environ["dockerMatchContainerName"]
+            del os.environ["DOCKER_MATCH_CONTAINER_NAME"]
 
     def test_include_and_exclude(self):
         try:
-            os.environ["dockerMatchContainerName"] = "container0, container1, container2"
-            os.environ["dockerSkipContainerName"] = "container0, container1, container2"
+            os.environ["DOCKER_MATCH_CONTAINER_NAME"] = "container0, container1, container2"
+            os.environ["DOCKER_SKIP_CONTAINER_NAME"] = "container0, container1, container2"
             self.assertRaises(KeyError, setup)
         finally:
-            del os.environ["dockerMatchContainerName"]
-            del os.environ["dockerSkipContainerName"]
+            del os.environ["DOCKER_MATCH_CONTAINER_NAME"]
+            del os.environ["DOCKER_SKIP_CONTAINER_NAME"]
 
     def test_period(self):
         try:
-            conf, name = setup()
+            conf = setup()
             self.assertEqual(conf[0]["period"], "10s")
 
-            period = os.environ["dockerPeriod"] = "1s"
-            conf, name = setup()
+            period = os.environ["DOCKER_PERIOD"] = "1s"
+            conf = setup()
             self.assertEqual(conf[0]["period"], period)
         finally:
-            del os.environ["dockerPeriod"]
+            del os.environ["DOCKER_PERIOD"]
 
     def test_certificate(self):
         try:
-            conf, name = setup()
+            conf = setup()
             self.assertRaises(KeyError, lambda: conf[0]["ssl"])
 
-            certificate_authority = os.environ["dockerCertificateAuthority"] = "/etc/pki/root/ca.pem"
-            certificate = os.environ["dockerCertificate"] = "/etc/pki/client/cert.pem"
-            key = os.environ["dockerKey"] = "/etc/pki/client/cert.key"
-            conf, name = setup()
+            certificate_authority = os.environ["DOCKER_CERTIFICATE_AUTHORITY"] = "/etc/pki/root/ca.pem"
+            certificate = os.environ["DOCKER_CERTIFICATE"] = "/etc/pki/client/cert.pem"
+            key = os.environ["DOCKER_KEY"] = "/etc/pki/client/cert.key"
+            conf = setup()
             self.assertEqual(conf[0]["ssl"]["certificate_authority"], certificate_authority)
             self.assertEqual(conf[0]["ssl"]["certificate"], certificate)
             self.assertEqual(conf[0]["ssl"]["key"], key)
         finally:
-            del os.environ["dockerCertificateAuthority"]
-            del os.environ["dockerCertificate"]
-            del os.environ["dockerKey"]
+            del os.environ["DOCKER_CERTIFICATE_AUTHORITY"]
+            del os.environ["DOCKER_CERTIFICATE"]
+            del os.environ["DOCKER_KEY"]
 
     def test_cpu_per_core(self):
         try:
-            conf, name = setup()
+            conf = setup()
             self.assertRaises(KeyError, lambda: conf[0]["cpu.cores"])
 
-            os.environ["dockerCPUPerCore"] = "true"
-            conf, name = setup()
+            os.environ["DOCKER_CPU_PER_CORE"] = "true"
+            conf = setup()
             self.assertEqual(conf[0]["cpu.cores"], "true")
         finally:
-            del os.environ["dockerCPUPerCore"]
+            del os.environ["DOCKER_CPU_PER_CORE"]
 
 
 if __name__ == '__main__':
